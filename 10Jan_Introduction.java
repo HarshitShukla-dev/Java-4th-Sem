14c4dd0650358f25bf75b58295d22ad19ef18cab
hershit-shukla <hershitshukla35@gmail.com>
2024-01-10 14:09:32 +0000

    Jan 10, 2024, 7:39 PM

--- 10Jan_Introduction.java
+++ 10Jan_Introduction.java
@@ -3,3 +3,8 @@ class HelloWorld {
         System.out.println("Hello, World!");
     }
 }
+
+//to run the code manually in terminal use the following commands in the same directory
+// javac 10Jan_Introduction.java
+// java HelloWorld
+
